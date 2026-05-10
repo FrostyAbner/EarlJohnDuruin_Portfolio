747189660511da2d9310cffa0353c02e91ef243f
Earl <earljohn211@gmail.com>
2026-05-10 14:19:55 +0800

    fix: correct base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/E.J.D-Portfolio/",
+  base: "/my_portfolio/",
   build: {
     outDir: "docs",
   },
